aaa134585e09c193fdde977e65f17eaf636e2384
ukfootprint <59648364+ukfootprint@users.noreply.github.com>
2020-02-11 09:31:12 +0000

    Update monthlyCronjob.sh

--- monthlyCronjob.sh
+++ monthlyCronjob.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Stop Nginx so Certbot can go ahead and run.
-systemctl stop nginx
+# Don't think this is necessary - SH
+#systemctl stop nginx
 
 # This script should be run at least every two months (i.e. <90 days) to auto-renew the Let's Encrypt SSL certificate.
 # Running more often (every day / week) doesn't do any harm, though.
@@ -10,4 +11,6 @@ certbot renew
 # After renewing, backup the new SSL certificate.
 #tar cf /root/...
 
-systemctl start nginx
+# No need to restart - reload instead
+#systemctl start nginx
+systemctl reload nginx
